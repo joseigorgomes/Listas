62f8a1ce7777d6f4e4f44df8e6ec6e65cebad971
RaissonSouto <raissonsouto@outlook.com>
2019-10-09 22:53:10 -0300

    ordenalista.py
    Nesse commit eu coloquei o insertion sort, um método mais eficiente de ordenar listas.

--- ordenalista.py
+++ ordenalista.py
@@ -1,20 +1,21 @@
 class OrdenaLista():
 
-    def ordenaDecrescente(lista):
-        auxiliar = 0
-        for i in range(len(lista)):
-            for j in range(len(lista)):
-                if lista[i] > lista[j]:
-                    auxiliar = lista[j]
-                    lista[j] = lista[i]
-                    lista[i] = auxiliar
+    def ordenaDecrescente(lista):                   
+        for i in range(1, len(lista)): 
+            key = lista[i] 
 
+            j = i-1
+            while j >=0 and key < lista[j] : 
+                lista[j+1] = lista[j] 
+                j -= 1
+            lista[j+1] = key 
 
     def ordenaCrescente(lista):
-        auxiliar = 0
-        for i in range(len(lista)):
-            for j in range(len(lista)):
-                if lista[i] < lista[j]:
-                    auxiliar = lista[j]
-                    lista[j] = lista[i]
-                    lista[i] = auxiliar
+        for j in range(1,len(lista)):
+            key = lista[j]
+
+            i = j - 1
+            while i >= 0 and lista[i] < key:
+                lista[i+1] = lista[i];
+                i-=1
+            lista[i+1] = key
